apache/incubator-kie-drools

contextual conversations

drools-core/src/main/java/org/drools/core/command/EndConversationCommand.java
@@ -1,0 +1,25 @@
+package org.drools.core.command;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.kie.internal.command.Context;
+
+
+public class EndConversationCommand<Void> implements GenericCommand<Void> {
+    private long conversationId;
+
+    public EndConversationCommand() {
+    }
+
+    public EndConversationCommand(long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    @Override
+    public Void execute(Context context) {
+        RequestContextImpl reqContext = (RequestContextImpl)context;
+        ConversationManager cvnManager = reqContext.getConversationManager();
+        cvnManager.endConversation(reqContext, conversationId);
+
+        return (Void) null;
+    }
+}

drools-core/src/main/java/org/drools/core/command/JoinConversationCommand.java
@@ -1,0 +1,22 @@
+package org.drools.core.command;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.kie.internal.command.Context;
+
+
+public class JoinConversationCommand<Void> implements GenericCommand<Void> {
+    private long conversationId;
+
+    public JoinConversationCommand(long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    @Override
+    public Void execute(Context context) {
+        RequestContextImpl reqContext = (RequestContextImpl)context;
+        ConversationManager cvnManager = reqContext.getConversationManager();
+        cvnManager.joinConversation(reqContext, conversationId);
+
+        return (Void) null;
+    }
+}

tarilabs/droolstake2

newdir/drools-core/src/main/java/org/drools/core/command/EndConversationCommand.java
@@ -1,0 +1,25 @@
+package org.drools.core.command;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.kie.internal.command.Context;
+
+
+public class EndConversationCommand<Void> implements GenericCommand<Void> {
+    private long conversationId;
+
+    public EndConversationCommand() {
+    }
+
+    public EndConversationCommand(long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    @Override
+    public Void execute(Context context) {
+        RequestContextImpl         reqContext = (RequestContextImpl)context;
+        ConversationContextManager cvnManager = reqContext.getConversationManager();
+        cvnManager.endConversation(reqContext, conversationId);
+
+        return (Void) null;
+    }
+}

newdir/drools-core/src/main/java/org/drools/core/command/JoinConversationCommand.java
@@ -1,0 +1,22 @@
+package org.drools.core.command;
+
+import org.drools.core.command.impl.GenericCommand;
+import org.kie.internal.command.Context;
+
+
+public class JoinConversationCommand<Void> implements GenericCommand<Void> {
+    private long conversationId;
+
+    public JoinConversationCommand(long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    @Override
+    public Void execute(Context context) {
+        RequestContextImpl         reqContext = (RequestContextImpl)context;
+        ConversationContextManager cvnManager = reqContext.getConversationManager();
+        cvnManager.joinConversation(reqContext, conversationId);
+
+        return (Void) null;
+    }
+}